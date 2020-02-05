bdc62aa92c39002817d443aded6037b5ca0be523
wdbm <wbm@protonmail.ch>
2020-02-05 15:56:30 +0000

    change to more PEP8

--- setup.py
+++ setup.py
@@ -6,36 +6,36 @@ import setuptools
 
 def main():
     setuptools.setup(
-        name                 = "python_spin",
-        version              = "2018.08.07.2245",
-        description          = "a small utility to assist in setting usage modes of laptop-tablet devices",
+        name                 = 'python_spin',
+        version              = '2020.02.05.1555',
+        description          = 'a small utility to assist in setting usage modes of laptop-tablet devices',
         long_description     = long_description(),
-        url                  = "https://github.com/wdbm/spin",
-        author               = "Will Breaden Madden",
-        author_email         = "wbm@protonmail.ch",
-        license              = "GPLv3",
+        url                  = 'https://github.com/wdbm/spin',
+        author               = 'Will Breaden Madden',
+        author_email         = 'wbm@protonmail.ch',
+        license              = 'GPLv3',
         packages             = setuptools.find_packages(),
         install_requires     = [
-                               "docopt",
-                               "technicolor"
+                               'docopt',
+                               'technicolor'
                                ],
         entry_points         = {
-                               "console_scripts": ("spin = python_spin.__init__:main")
+                               'console_scripts': ('spin = python_spin.__init__:main')
                                },
         include_package_data = True,
         zip_safe             = False
     )
 
-def long_description(filename = "README.md"):
+def long_description(filename = 'README.md'):
     if os.path.isfile(os.path.expandvars(filename)):
       try:
           import pypandoc
-          long_description = pypandoc.convert_file(filename, "rst")
+          long_description = pypandoc.convert_file(filename, 'rst')
       except ImportError:
           long_description = open(filename).read()
     else:
-        long_description = ""
+        long_description = ''
     return long_description
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
